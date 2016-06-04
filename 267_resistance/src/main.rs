d4d1e0ad52dfc3857d8dae228cb231cb0cca9763
Andrew Thorburn <nzipsi@gmail.com>
2016-06-04 22:41:14 +0100

    Compiles for real.
    Doing some refactoring to make the borrow checker happy (happier,
    anyway)

--- 267_resistance/src/main.rs
+++ 267_resistance/src/main.rs
@@ -6,6 +6,62 @@ use std::collections::HashMap;
 use std::cmp::{Ord, Ordering};
 use std::process::exit;
 
+struct Grid {
+    node_map: HashMap<String, Node>,
+    link_list: Vec<Link>,
+}
+
+impl Grid {
+    fn new(line: String) -> Grid {
+        let mut node_map = HashMap::new(); 
+        let nodes = line.split_whitespace(); 
+        for n in nodes {
+            node_map.insert(n.to_string(), Node::new(n.to_string()));
+        }
+        Grid { node_map: node_map, link_list: Vec::new() }
+    }
+
+    fn parse_line(&mut self, line: String) -> Result<()> {
+        let node_details: Vec<&str> = line.split_whitespace().collect();
+        let node1_name = node_details[0].to_string();
+        let node2_name = node_details[1].to_string();
+        let mut node1: Node;
+        let mut node2: Node;
+        match self.node_map.remove(&node1_name) {
+            Some(n) => node1 = n,
+            None => {
+                try!(write!(std::io::stderr(), "Unable to find node {}!\n", &node1_name));
+                exit(1);
+            },
+        };
+        match self.node_map.remove(&node2_name) {
+            Some(n) => node2 = n,
+            None => {
+                try!(write!(std::io::stderr(), "Unable to find node {}!\n", &node2_name));
+                exit(1);
+            },
+        };
+        let resistence = node_details[2].parse::<u16>().unwrap();
+        match node1.name.cmp(&node2.name) {
+            Ordering::Less | Ordering::Equal => { 
+                self.link_list.push(Link::new(resistence, node1_name.to_owned(), node2_name.to_owned()));
+                
+                node1.add_target(self.link_list.len());
+                node2.add_source(self.link_list.len());
+            },
+            Ordering::Greater => {
+                self.link_list.push(Link::new(resistence, node2_name.to_owned(), node1_name.to_owned()));
+
+                node2.add_target(self.link_list.len());
+                node1.add_source(self.link_list.len());
+            },
+        }
+        self.node_map.insert(node1_name, node1);
+        self.node_map.insert(node2_name, node2);
+        Ok(())
+    }
+}
+
 struct Node {
     sources: Vec<usize>,
     targets: Vec<usize>,
@@ -42,22 +98,7 @@ impl Link {
     }
 }
 
-fn create_nodes(line: &String) -> HashMap<String, Node> {
-   let mut node_map = HashMap::new(); 
-            let nodes = line.split_whitespace(); 
-            for n in nodes {
-                let node_name = n.to_string();
-                node_map.insert(node_name.to_string(), Node::new(node_name.to_string()));
-                // start = match start.cmp(&node_name) {
-                //    Ordering::Less | Ordering::Equal => start,
-                //    Ordering::Greater => node_name,
-                // };
-            }
-            return node_map;
-}
-
 fn run() -> Result<()> {
-    // let mut start = String::from("~");
     let a: Vec<String> = env::args().collect();
     if a.len() != 2 {
         try!(std::io::stderr().write(b"Command must be called with file containing resistance description\n"));
@@ -68,72 +109,35 @@ fn run() -> Result<()> {
     let reader = BufReader::new(f);
 
     let mut i: u8 = 0;
-    let mut node_map = HashMap::new();
-    let mut link_list = Vec::new();
+    let mut header;
+    let mut lines = Vec::new();
     for line in reader.lines() {
         let line: String = try!(line);
         println!("[{}] - {}", i, line);
         if i == 0 {
-            node_map = create_nodes(&line);
+            header = line;
         } else {
-            let node_details: Vec<&str> = line.split_whitespace().collect();
-            let node1_name = node_details[0].to_string();
-            let node2_name = node_details[1].to_string();
-            let mut node1: Node;
-            let mut node2: Node;
-            match node_map.remove(&node1_name) {
-                Some(n) => node1 = n,
-                None => {
-                    try!(write!(std::io::stderr(), "Unable to find node {}!\n", &node1_name));
-                    exit(1);
-                },
-            };
-            match node_map.remove(&node2_name) {
-                Some(n) => node2 = n,
-                None => {
-                    try!(write!(std::io::stderr(), "Unable to find node {}!\n", &node2_name));
-                    exit(1);
-                },
-            };
-            let resistence = node_details[2].parse::<u16>().unwrap();
-            match node1.name.cmp(&node2.name) {
-                Ordering::Less | Ordering::Equal => { 
-                    let l = Link::new(resistence, node1_name.to_owned(), node2_name.to_owned());
-                    link_list.push(l);
-                    
-                    node1.add_target(link_list.len());
-                    node2.add_source(link_list.len());
-                },
-                Ordering::Greater => {
-                    let l = Link::new(resistence, node2_name.to_owned(), node1_name.to_owned());
-                    link_list.push(l);
-
-                    node2.add_target(link_list.len());
-                    node1.add_source(link_list.len());
-                },
-            }
-            node_map.insert(node1_name, node1);
-            node_map.insert(node2_name, node2);
+            lines.push(line);
         }
         i += 1;
     }
     // println!("Will start with node [{}]", start);
-    for (key, value) in node_map {
-        println!("{}", key);
-    }
+//    for (key, value) in node_map {
+//        println!("{}", key);
+//    }
     Ok(())
 }
 
 fn process_node(node: &mut Node) {
-    if node.targets.len() == 1 {
-        if node.sources.len() == 1 {
-            return;
-        } // else if node.targets[0].targetNode.sources.len() == 1 && node.targets[0].targetNode.targets.len() == 1 {
-            // let link = Link::new(node.targets[0].resistence + node.targets[0].targetNode.targets[0].resistence, node, node.targets[0].targetNode.targets[0].targetNode);
-            // node.targets[0].targetNode.targets[0].targetNode.sources[0] = &link;
-            // node.targets[0] = link;
-        // }
-    }
+//    if node.targets.len() == 1 {
+//        if node.sources.len() == 1 {
+//            return;
+//        } else if node.targets[0].targetNode.sources.len() == 1 && node.targets[0].targetNode.targets.len() == 1 {
+//            let link = Link::new(node.targets[0].resistence + node.targets[0].targetNode.targets[0].resistence, node, node.targets[0].targetNode.targets[0].targetNode);
+//            node.targets[0].targetNode.targets[0].targetNode.sources[0] = &link;
+//            node.targets[0] = link;
+//        }
+//    }
 }
 
 fn main() {
